65e554cebcd08bb605a74197096acd0e59802359
zxx <2458543125@qq.com>
2025-04-03 16:17:15 +0800

    提交决策树算法

--- 机器学习算法/决策树/decision_tree.py
+++ 机器学习算法/决策树/decision_tree.py
@@ -434,9 +434,9 @@ def createTreePrePruning(dataTrain, labelTrain, dataTest, labelTest, names, meth
 # 将西瓜数据2.0分割为测试集和训练集
 xgDataTrain, xgLabelTrain, xgDataTest, xgLabelTest = splitXgData20(xgData, xgLabel)
 # 生成不剪枝的树
-xgTreeTrain = createTree(xgDataTrain, xgLabelTrain, xgName, method = 'id3')
+xgTreeTrain = createTree(xgDataTrain, xgLabelTrain, xgName, method='id3')
 # 生成预剪枝的树
-xgTreePrePruning = createTreePrePruning(xgDataTrain, xgLabelTrain, xgDataTest, xgLabelTest, xgName, method = 'id3')
+xgTreePrePruning = createTreePrePruning(xgDataTrain, xgLabelTrain, xgDataTest, xgLabelTest, xgName, method='id3')
 # 画剪枝前的树
 print("剪枝前的树")
 createPlot(xgTreeTrain)
@@ -444,3 +444,127 @@ createPlot(xgTreeTrain)
 print("剪枝后的树")
 createPlot(xgTreePrePruning)
 
+
+# 创建决策树 带预划分标签
+def createTreeWithLabel(data, labels, names, method='id3'):
+    data = np.asarray(data)
+    labels = np.asarray(labels)
+    names = np.asarray(names)
+    # 如果不划分的标签为
+    votedLabel = voteLabel(labels)
+    # 如果结果为单一结果
+    if len(set(labels)) == 1:
+        return votedLabel
+        # 如果没有待分类特征
+    elif data.size == 0:
+        return votedLabel
+    # 其他情况则选取特征
+    bestFeat, bestEnt = bestFeature(data, labels, method=method)
+    # 取特征名称
+    bestFeatName = names[bestFeat]
+    # 从特征名称列表删除已取得特征名称
+    names = np.delete(names, [bestFeat])
+    # 根据选取的特征名称创建树节点 划分前的标签votedPreDivisionLabel=_vpdl
+    decisionTree = {bestFeatName: {"_vpdl": votedLabel}}
+    # 根据最优特征进行分割
+    dataSet, labelSet = splitFeatureData(data, labels, bestFeat)
+    # 对最优特征的每个特征值所分的数据子集进行计算
+    for featValue in dataSet.keys():
+        decisionTree[bestFeatName][featValue] = createTreeWithLabel(dataSet.get(featValue), labelSet.get(featValue),
+                                                                    names, method)
+    return decisionTree
+
+
+# 将带预划分标签的tree转化为常规的tree
+# 函数中进行的copy操作，原因见有道笔记 【YL20190621】关于Python中字典存储修改的思考
+def convertTree(labeledTree):
+    labeledTreeNew = labeledTree.copy()
+    nodeName = list(labeledTree.keys())[0]
+    labeledTreeNew[nodeName] = labeledTree[nodeName].copy()
+    for val in list(labeledTree[nodeName].keys()):
+        if val == "_vpdl":
+            labeledTreeNew[nodeName].pop(val)
+        elif type(labeledTree[nodeName][val]) == dict:
+            labeledTreeNew[nodeName][val] = convertTree(labeledTree[nodeName][val])
+    return labeledTreeNew
+
+
+# 后剪枝 训练完成后决策节点进行替换评估  这里可以直接对xgTreeTrain进行操作
+def treePostPruning(labeledTree, dataTest, labelTest, names):
+    newTree = labeledTree.copy()
+    dataTest = np.asarray(dataTest)
+    labelTest = np.asarray(labelTest)
+    names = np.asarray(names)
+    # 取决策节点的名称 即特征的名称
+    featName = list(labeledTree.keys())[0]
+    # print("\n当前节点：" + featName)
+    # 取特征的列
+    featCol = np.argwhere(names == featName)[0][0]
+    names = np.delete(names, [featCol])
+    # print("当前节点划分的数据维度：" + str(names))
+    # print("当前节点划分的数据：" )
+    # print(dataTest)
+    # print(labelTest)
+    # 该特征下所有值的字典
+    newTree[featName] = labeledTree[featName].copy()
+    featValueDict = newTree[featName]
+    featPreLabel = featValueDict.pop("_vpdl")
+    # print("当前节点预划分标签：" + featPreLabel)
+    # 是否为子树的标记
+    subTreeFlag = 0
+    # 分割测试数据 如果有数据 则进行测试或递归调用  np的array我不知道怎么判断是否None, 用is None是错的
+    dataFlag = 1 if sum(dataTest.shape) > 0 else 0
+    if dataFlag == 1:
+        # print("当前节点有划分数据！")
+        dataTestSet, labelTestSet = splitFeatureData(dataTest, labelTest, featCol)
+    for featValue in featValueDict.keys():
+        # print("当前节点属性 {0} 的子节点：{1}".format(featValue ,str(featValueDict[featValue])))
+        if dataFlag == 1 and type(featValueDict[featValue]) == dict:
+            subTreeFlag = 1
+            # 如果是子树则递归
+            newTree[featName][featValue] = treePostPruning(featValueDict[featValue], dataTestSet.get(featValue),
+                                                           labelTestSet.get(featValue), names)
+            # 如果递归后为叶子 则后续进行评估
+            if type(featValueDict[featValue]) != dict:
+                subTreeFlag = 0
+
+                # 如果没有数据  则转换子树
+        if dataFlag == 0 and type(featValueDict[featValue]) == dict:
+            subTreeFlag = 1
+            # print("当前节点无划分数据！直接转换树："+str(featValueDict[featValue]))
+            newTree[featName][featValue] = convertTree(featValueDict[featValue])
+            # print("转换结果：" + str(convertTree(featValueDict[featValue])))
+    # 如果全为叶子节点， 评估需要划分前的标签，这里思考两种方法，
+    #     一是，不改变原来的训练函数，评估时使用训练数据对划分前的节点标签重新打标
+    #     二是，改进训练函数，在训练的同时为每个节点增加划分前的标签，这样可以保证评估时只使用测试数据，避免再次使用大量的训练数据
+    #     这里考虑第二种方法 写新的函数 createTreeWithLabel，当然也可以修改createTree来添加参数实现
+    if subTreeFlag == 0:
+        ratioPreDivision = equalNums(labelTest, featPreLabel) / labelTest.size
+        equalNum = 0
+        for val in labelTestSet.keys():
+            equalNum += equalNums(labelTestSet[val], featValueDict[val])
+        ratioAfterDivision = equalNum / labelTest.size
+        # print("当前节点预划分标签的准确率：" + str(ratioPreDivision))
+        # print("当前节点划分后的准确率：" + str(ratioAfterDivision))
+        # 如果划分后的测试数据准确率低于划分前的，则划分无效，进行剪枝，即使节点等于预划分标签
+        # 注意这里取的是小于，如果有需要 也可以取 小于等于
+        if ratioAfterDivision < ratioPreDivision:
+            newTree = featPreLabel
+    return newTree
+
+
+# 书中的树结构 p81 p83
+xgTreeBeforePostPruning = {"脐部": {"_vpdl": "是"
+    , '凹陷': {'色泽': {"_vpdl": "是", '青绿': '是', '乌黑': '是', '浅白': '否'}}
+    , '稍凹': {'根蒂': {"_vpdl": "是"
+        , '稍蜷': {'色泽': {"_vpdl": "是"
+            , '青绿': '是'
+            , '乌黑': {'纹理': {"_vpdl": "是"
+                , '稍糊': '是', '清晰': '否', '模糊': '是'}}
+            , '浅白': '是'}}
+        , '蜷缩': '否'
+        , '硬挺': '是'}}
+    , '平坦': '否'}}
+xgTreePostPruning = treePostPruning(xgTreeBeforePostPruning, xgDataTest, xgLabelTest, xgName)
+createPlot(convertTree(xgTreeBeforePostPruning))
+createPlot(xgTreePostPruning)
